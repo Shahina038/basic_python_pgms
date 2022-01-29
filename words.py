efd272e909bdc2847b28cefad7cd1bbdbcf99371
Shahina038 <98080803+Shahina038@users.noreply.github.com>
2022-01-29 08:37:25 +0530

    Update words.py

--- words.py
+++ words.py
@@ -2,3 +2,12 @@
 def words(txt):
     return len(txt.split())
 print(words('Python is a programming language'))
+
+
+# number of sentences in a string
+import re
+def sentence_count(txt):
+    return len(re.split('\.|\?',txt))
+string="I am a programmer. What do I do? I program. Which language do I program in? Why, Python of course!"
+print(sentence_count(string))
+
