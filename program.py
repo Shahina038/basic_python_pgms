a378889568d32efe233357319a01521bcd31182e
Shahina038 <98080803+Shahina038@users.noreply.github.com>
2022-01-29 10:39:42 +0530

    Update and rename words.py to program.py

--- program.py
+++ program.py
@@ -11,3 +11,9 @@ def sentence_count(txt):
 string="I am a programmer. What do I do? I program. Which language do I program in? Why, Python of course!"
 print(sentence_count(string))
 
+# function to return most frequent number
+def most_frequent(List):
+    return max(set(List), key = List.count)
+ 
+List = [1, 4, 2, 4, 1, 4,2]
+print(most_frequent(List))
