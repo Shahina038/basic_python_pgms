a806e6c3db150f3d71c14f7003f987d63d800e64
Shahina038 <98080803+Shahina038@users.noreply.github.com>
2022-02-03 20:08:20 +0530

    Update programs.py

--- programs.py
+++ programs.py
@@ -19,3 +19,14 @@ def most_frequent(List):
     return max(set(List), key = List.count)
 List = [1, 4, 2, 4, 1, 4,2]
 print(most_frequent(List))
+
+
+# function that will remove everything except string ina given list
+def string_list(items):
+    string = []
+    for item in items:
+        if type(item) == str:
+            string.append(item)
+    return string
+items = [1,'python','code',3,'pgm']
+print(string_list(items))
